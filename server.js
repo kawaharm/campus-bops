6ca6e76f64949f6c497973a3bf19854bef7d8de8
kawaharm <mjbk295@gmail.com>
2022-02-11 13:49:35 -0800

    created brackets

--- server.js
+++ server.js
@@ -87,6 +87,18 @@ app.get('/profile', isLoggedIn, (req, res) => {
     })
 });
 
+app.get('/bracket', (req, res) => {
+  School.findAll()
+    .then(function (schoolList) {
+      res.render('brackets/index', { schools: schoolList })
+    })
+    .catch(function (err) {
+      console.log('ERROR', err);
+      res.json({ message: 'Error occured, please try again....' });
+    });
+})
+
+
 // CONTROLLERS
 app.use('/auth', require('./controllers/auth'));
 app.use('/categories', require('./controllers/categories'));
